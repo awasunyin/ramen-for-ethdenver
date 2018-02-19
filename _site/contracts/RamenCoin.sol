1d3fa801ae9e6e9b423303431e2564b575070551
awasunyin <awasunyin@gmail.com>
2018-02-18 17:04:12 -0700

    removed all the unnecessary jekyll files

--- _site/contracts/RamenCoin.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.17;
-
-import "./OpenZeppelin/ERC20.sol";
-
-// TODO: RamenCoin used as currency for playing in the game
-
-contract RamenCoin is ERC20 {
-
-}
--- posts/index.md
+++
@@ -1,6 +0,0 @@
----
-layout: post-list
-title: All Posts
-excerpt: "Latest posts"
-comments: false
----
